ef3a2499cb700f074466e38765ac12e3e3ab473c
CooDDiiNNgg <niki@valkanovi.com>
2023-05-02 20:03:06 +0300

    Get_access_for_FireFox

---
+++ Get_Access/FireFox/Get_Access.js
@@ -0,0 +1,43 @@
+browser.browserAction.onClicked.addListener(function(tab) {
+    browser.tabs.reload();
+    browser.webRequest.onHeadersReceived.addListener(
+        logResponseData,
+        { urls: ["<all_urls>"]},
+        ["responseHeaders", "blocking"]
+    );
+});
+
+  
+  function logResponseData(details) {
+    if (details.method === "POST" && details.url.includes("tQS")) {      
+        if(details.url.split("_reqid=")[1].split("&")[0].startsWith("2") || details.url.split("_reqid=")[1].split("&")[0].startsWith("3")){
+            
+            let responseStream = browser.webRequest.filterResponseData(details.requestId);
+            let decoder = new TextDecoder();
+            let responseText = "";
+
+            responseStream.ondata = event => {
+                responseText += decoder.decode(event.data, {stream: true});
+                responseStream.write(event.data);
+            };
+            responseStream.onstop = event => {
+                if(responseText.search("https://drive.google.com") != -1){
+                    var url = responseText.split("https://drive.google.com")[1].split('"')[0];
+                    url = "https://drive.google.com" + url;
+                    console.log(url);
+                    browser.tabs.create({url: url});
+                }
+                responseStream.close();
+                Delete_listeners();
+            }
+         }
+  
+        return {};
+    }
+}
+
+
+function Delete_listeners(){
+    browser.webRequest.onHeadersReceived.removeListener(logResponseData);
+}
+  
